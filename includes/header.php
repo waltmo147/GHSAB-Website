6afb469061939dedf380b68409f745daa8ac7fa8
Walt Mo <apple@dhcp-rhodes-3249.eduroam.cornell.edu>
2018-04-28 19:15:56 -0400

    header

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header>
+  <h1><?php echo "Title"; ?></h1>
+</header>
--- index.php
+++ index.php
@@ -6,11 +6,12 @@ $current_page = "Home"?>
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" type="text/css" href="styles.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
   <title>Home</title>
 </head>
 
 <body>
 <?php include('includes/sidebar.php'); ?>
+<?php include('includes/header.php'); ?>
 </body>
 </html>
