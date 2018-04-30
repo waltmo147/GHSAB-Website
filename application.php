0b2fd8c7a24ccb09f3a0dcb2cf7692e782289ec6
Jing Guo <jingguo@dhcp-rhodes-113.eduroam.cornell.edu>
2018-04-30 16:17:46 -0400

    add apply button to events

--- application.php
+++ application.php
@@ -1,7 +1,47 @@
 <?php include('includes/init.php');
 $current_page = "Events";
+$current_event_id = filter_input(INPUT_GET, "event_id", FILTER_VALIDATE_INT);
 //get all events
-$allevents =  exec_sql_query($db, "SELECT * FROM events", NULL)->fetchAll();
+$current_event =  exec_sql_query($db, "SELECT * FROM events where id is :event_id", NULL)->fetchAll();
+
+
+if (isset($_POST["submit"])) {
+  //validate input
+  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+  $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+  //get tag_id
+  $sql = "SELECT * from applications where event_id is :event_id and email is :email";
+  $params = array(
+    ":event_id" => $current_event_id,
+    ":email" => $email
+  );
+  $result = exec_sql_query($db, $sql, $params)->fetchAll();
+  if ($result) {
+    $tag = $result[0];
+    $new_tag_id = $tag["id"];
+  } else {
+    $sql = "INSERT INTO tags (name) VALUES (:new_tag)";
+    $params = array(':new_tag' => $new_tag);
+    $result = exec_sql_query($db, $sql, $params);
+    if ($result) {
+      $new_tag_id = $db->lastInsertId("id");
+    }
+  }
+  // add painting tag
+  $sql = "INSERT INTO painting_tag (painting_id, tag_id)
+  VALUES (:painting_id, :new_tag_id)";
+  $params = array(
+    ":painting_id" => $current_painting,
+    ":new_tag_id" => $new_tag_id
+  );
+  $result = exec_sql_query($db, $sql, $params)->fetchAll();
+  if ($result) {
+    echo "New tag has been added.";
+  } else {
+    echo "Failed to add new tag.";
+  }
+}
+
 
 function print_events($events) {
   ?>
@@ -12,7 +52,6 @@ function print_events($events) {
       <th>Time</th>
       <th>Address</th>
       <th>Description</th>
-      <th>Application</th>
     </tr>
   <?php
   foreach ($events as $event) {
@@ -35,13 +74,6 @@ function print_events($events) {
       <td> <?php echo htmlspecialchars($event["date_time"]); ?> </td>
       <td> <?php echo htmlspecialchars($event["address"]); ?> </td>
       <td> <?php echo htmlspecialchars($event["description"]); ?> </td>
-      <td>
-        <?php
-        echo "<form class=\"loginform\" action=\"application.php?event_id=".$event['id']."\" method=\"post\">";
-        ?>
-          <button type="submit" name="apply">Apply</button>
-        </form>
-      </td>
     </tr>
     <?php
   }
@@ -67,7 +99,7 @@ include('includes/sidebar.php');
 <div class='show_events'>
   <h3>Upcomming Events</h3>
   <?php
-      print_events($allevents);
+      print_events($current_event);
   ?>
 </div>
 <?php include('includes/footer.php')?>
