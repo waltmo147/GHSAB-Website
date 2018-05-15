4142c329b9d4e6c5b029ccbb4039772b0aa45f53
Walt Mo <apple@dhcp-rhodes-2870.eduroam.cornell.edu>
2018-05-14 22:05:22 -0400

    admin event css change

--- admin_event.php
+++ admin_event.php
@@ -211,7 +211,7 @@ if ($current_user) {
 <div class='event_form'>
 
 
-  <form class='loginform' method="post" action="admin_event.php" enctype="multipart/form-data">
+  <form class='eventform' method="post" action="admin_event.php" enctype="multipart/form-data">
     <fieldset>
       <legend>Event form</legend>
       <?php print_messages(); ?>
@@ -237,6 +237,8 @@ if ($current_user) {
         </li>
         <li>
           <label>Upload Image:</label>
+        </li>
+        <li>
         <!-- MAX_FILE_SIZE must precede the file input field -->
           <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MAX_FILE_SIZE; ?>" />
           <input type="file" name="box_file" >
