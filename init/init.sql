146361109840b87374d9ee742a3b6757eeaac3a8
unknown <ema82@cornell.edu>
2018-05-15 21:07:50 -0400

    new pics

--- init/init.sql
+++ init/init.sql
@@ -72,10 +72,10 @@ INSERT INTO picliason (member, picture) VALUES ((SELECT id FROM members WHERE fi
 INSERT INTO admin (username, password) VALUES ('admin', '$2y$10$i41U0Al7AUAQFIAeoi0NH.1Aykum/Hf3vScM.zt1pQKcKHDSPlm0e');
 
 -- seed data for slideshow
-INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/bald-eagle.jpg', "Bald Eagle");
-INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/bald-eagle-1.jpg', "Bald Eagle");
-INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/bird.jpg', "Bird");
-INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/blackbirds.jpg', "BlackBirds");
+INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/image1.jpg', "image1");
+INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/image2.jpg', "image2");
+INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/image3.jpg', "image3");
+INSERT INTO slideshow (picpath, title) VALUES ('uploads/pictures/image4.jpg', "image4");
 
 INSERT INTO events (name, date_time, address, description) values ('Cornell Ochestra', '2018-04-29 15:00:00', 'Bailey Hall', 'Tchaikovsky');
 INSERT INTO application (event_id, email, comment) values ((SELECT id FROM events where name is 'Cornell Ochestra'), 'jg2273@cornell.edu', 'Is is for free?');
